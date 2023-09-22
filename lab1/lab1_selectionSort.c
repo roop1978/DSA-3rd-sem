42688ff7861579d3425388af52b747d82c477b74
roop1978 <juneja.roopanshi@gmail.com>
2023-09-22 14:04:57 +0530

    .

--- lab1/lab1_selectionSort.c
+++ lab1/lab1_selectionSort.c
@@ -12,25 +12,30 @@ void main()
      for(k=0;k<n;k++){
         scanf("%d",&a[k]);
      }
-     for(i=0;i<n;i++)
+     for(i=0;i<n-1;i++)
      {
+         int mini=i;
          for(j=i+1;j<n;j++){
-            if(a[j]>a[i])
+            if(a[j]<a[mini])
             {
+                mini=j;
+            }
+            if(mini!=i){
                 temp=a[j];
                 a[j]=a[i];
                 a[i]=temp;
             }
+            }
          }
 
-     }
+
 
 
 
 
 for(k=0;k<n;k++)
 {
-    printf("%d",a[k]);
+    printf("%d\t",a[k]);
 }
 }
 
